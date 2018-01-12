1e0cc11dfff6a0485c0fd9d5a54cf632381f3abd
Pedro <pierreamaury@outlook.fr>
2018-01-12 17:00:05 +0100

    Ajout table Hrm
    Mise a jour des tables
    Ajout d'un modele

---
+++ kernel/app/model/Hrm.php
@@ -0,0 +1,172 @@
+<?php
+require_once(LIB."Model.php");
+class Hrm extends Model {
+    protected $id_hrm;
+    protected $username;
+    protected $firstname;
+    protected $lastname;
+    protected $email;
+    protected $address;
+    protected $city;
+    protected $postalcode;
+    protected $password;
+    public function __construct(){
+        parent::__construct("candidate","id_candidate");
+        $this->id_candidate = null;
+        $this->username = "";
+        $this->firstname = "";
+        $this->lastname = "";
+        $this->email = "";
+        $this->address = "";
+        $this->city = "";
+        $this->postalcode = "";
+        $this->password = "";
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return null
+     */
+    public function getIdCandidate()
+    {
+        return $this->id_candidate;
+    }
+
+    /**
+     * @param null $id_candidate
+     */
+    public function setIdCandidate($id_candidate)
+    {
+        $this->id_candidate = $id_candidate;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param string $firstname
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param mixed $lastname
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param string $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPostalcode()
+    {
+        return $this->postalcode;
+    }
+
+    /**
+     * @param string $postalcode
+     */
+    public function setPostalcode($postalcode)
+    {
+        $this->postalcode = $postalcode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+
+
+}
--- kernel/data/tables.sql
+++ kernel/data/tables.sql
@@ -8,6 +8,8 @@ CREATE TABLE "candidate"(
     postalcode varchar(5) NOT NULL,
     username varchar (20) NOT NULL UNIQUE,
     password varchar(250) NOT NULL,
+    created TIMESTAMP WITHOUT TIMEZONE,
+    updated TIMESTAMP WITHOUT TIMEZONE,
     CONSTRAINT pk_candidate PRIMARY KEY(id_candidate)
 );
 CREATE TABLE "experience"(
@@ -16,4 +18,18 @@ CREATE TABLE "experience"(
     label varchar(100) NOT NULL,
     CONSTRAINT pk_experience PRIMARY KEY (id_experience),
     CONSTRAINT fk_candidate FOREIGN KEY (candidate) REFERENCES candidate(id_candidate)
+);
+CREATE TABLE "hrm"(
+    id_hrm SERIAL,
+    username varchar (20) NOT NULL UNIQUE,
+    password varchar(250) NOT NULL,
+    firstname varchar(20) NOT NULL,
+    lastname varchar(20) NOT NULL,
+    email varchar(70) NOT NULL,
+    address varchar(100) NOT NULL,
+    city varchar(50) NOT NULL,
+    postalcode varchar(5) NOT NULL,
+    created TIMESTAMP WITHOUT TIMEZONE,
+    updated TIMESTAMP WITHOUT TIMEZONE,
+    CONSTRAINT pk_drh PRIMARY KEY(id_hrm)
 );
